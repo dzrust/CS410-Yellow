11788a785f03a8d0d7496aace2c0a175353516aa
Daniel <danielzrust@gmail.com>
2019-02-12 19:15:07 -0700

    Started working on style changes

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,20 +1,8 @@
 import * as React from "react";
 
 const Home = () => (
-    <div id="banner">
-        <div className="inner">
-            <header>
-                <h1>Skadoosh!!</h1>
-            </header>
-            <div className="flex ">
-                <div />
-                <div />
-                <div />
-            </div>
-            <footer>
-                <a></a>
-            </footer>
-        </div>
+    <div className="home-page">
+        <h1>Skadoosh!!</h1>
     </div>
 );
 
