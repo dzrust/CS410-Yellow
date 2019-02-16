bff2e881c2fae3114c1bf9e6a9fd8ccabcf50518
jquiamba <jquia004@odu.edu>
2019-02-16 15:31:33 -0500

    Source 1
    Testing How I put the references in

--- src/app/references.tsx
+++ src/app/references.tsx
@@ -5,6 +5,9 @@ const References = () => (
         <h1>
             References
         </h1>	
+        {
+        text: "Bradley Corporation. "Americans Come Clean on Messy Public Restrooms in New Survey." PR Newswire: News distribution, targeting and monitoring. 18 Sept. 2013. 13 Feb. 2019 <https://www.prnewswire.com/news-releases/americans-come-clean-on-messy-public-restrooms-in-new-survey-224213141.html>",
+        }
     </div>
 );
 
