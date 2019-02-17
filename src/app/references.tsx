5ddc06d1852516b63b129310741d00c23a30a1d2
jquiamba <jquia004@odu.edu>
2019-02-17 17:13:00 -0500

    References

--- src/app/references.tsx
+++ src/app/references.tsx
@@ -1,30 +1,13 @@
 import * as React from "react";
 
 const references = [
-    {
-        text: "Cleveland Clinic",
-        link: "https://my.clevelandclinic.org/health/articles/7040-gastrointestinal-disorders"
-    },
-    {
-        text: "Enviromaster Of Washington DC",
-        link: "http://www.enviromasterofwashingtondc.com/blog/dirty-public-restroom-floors-can-infect-entire-building/",
-    },
-    {
-        text: "Healthline",
-        link: "https://www.healthline.com/health/stress-constipation"
-    },
-    {
-        text: "PR News Wire",
-        link: "https://www.prnewswire.com/news-releases/americans-come-clean-on-messy-public-restrooms-in-new-survey-224213141.html",
-    },
-    {
-        text: "The Conversation",
-        link: "https://theconversation.com/why-do-public-bathrooms-make-us-so-anxious-and-why-arent-we-doing-anything-about-it-50107",
-    },
-    {
-        text: "Womens Health Mag",
-        link: "https://www.womenshealthmag.com/health/a19998266/pooping-at-work/",
-    },
+
+    'Bradley Corporation. "Americans Come Clean on Messy Public Restrooms in New Survey." PR Newswire: News distribution, targeting and monitoring. 18 Sept. 2013. 13 Feb. 2019 <https://www.prnewswire.com/news-releases/americans-come-clean-on-messy-public-restrooms-in-new-survey-224213141.html>.',
+    '"Digestive Disorders & Gastrointestinal Diseases." Cleveland Clinic. 13 Feb. 2019 <https://my.clevelandclinic.org/health/articles/7040-gastrointestinal-disorders>.',
+    '"How Dirty Public Restroom Floors Can Infect Your Entire Building." Enviro-Master of Washington DC. 07 Sept. 2017. 13 Feb. 2019 <http://www.enviromasterofwashingtondc.com/blog/dirty-public-restroom-floors-can-infect-entire-building/>.',
+    'Molotch, Harvey. "Why do public bathrooms make us so anxious, and why aren\'t we doing anything about it?" The Conversation. 13 Jan. 2019. The Conversation. 13 Feb. 2019 <https://theconversation.com/why-do-public-bathrooms-make-us-so-anxious-and-why-arent-we-doing-anything-about-it-50107>.',
+    'Person. "This Is How Many People Refuse to Poop at Work." Women\'s Health. 25 May 2018. Women\'s Health. 13 Feb. 2019 <https://www.womenshealthmag.com/health/a19998266/pooping-at-work/>.',
+    '"Stress Constipation: Causes and Treatments." Healthline. Healthline Media. 13 Feb. 2019 <https://www.healthline.com/health/stress-constipation>.',
 ];
 
 const References = () => (
@@ -36,9 +19,7 @@ const References = () => (
             {
                 references.map((reference) => (
                     <div>
-                        <a href={reference.link}>
-                            {reference.text}
-                        </a>
+                        {reference}
                     </div>
                 ))
             }
