af40b7e5a796a69a9b1c79974ac124402b0f2eda
bryanperez <bmesi002@odu.edu>
2019-02-20 11:11:36 -0500

    Update presentation.tsx

--- src/app/presentation.tsx
+++ src/app/presentation.tsx
@@ -4,7 +4,6 @@ const Presentation = () => (
     <div>
         <h1>
             Presentation
-            </br>
                  <p>
                     <iframe src="https://olddominion-my.sharepoint.com/personal/dzrus001_odu_edu/_layouts/15/Doc.aspx?sourcedoc={f8761093-7d98-4e70-9aa8-7f710c6beef3}&amp;action=embedview&amp;wdAr=1.7777777777777777" width="962px" height="565px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office Online</a>.</iframe>
                 </p>
