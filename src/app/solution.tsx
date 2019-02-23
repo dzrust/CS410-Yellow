8c8a89a0419254c2063e571d2c2d3ef1b27f6d2b
Bryan Perez <34199830+bryanperez43@users.noreply.github.com>
2019-02-23 15:11:29 -0500

    Update solution.tsx (#23)
    Temporary solution statement along with its characteristics
    -pending further changes

--- src/app/solution.tsx
+++ src/app/solution.tsx
@@ -1,10 +1,39 @@
 import * as React from "react";
 
+const Charateristics = [
+        "A smart application that located restrooms within your area",
+        "Allow users to filter restrooms with wanted needs",
+        "Provide location of gender neutrality",
+        "Help users avoid dirty restrooms",
+        "Help with unusual long wait times (maybe in stadiums)",
+    ];
+
 const Solution = () => (
     <div>
-        <h1>
-            Solution
-        </h1>	
+            
+                <h1>
+                    Solution Statement    
+                </h1>
+                <p>
+                Skadoosh will allow users the ability to locate, 
+                provide feedback, and to safely use public restrooms 
+                within your surrounding area.
+                </p>
+                <h2>
+                    Solution Charateristics:
+                </h2>
+                <ul>
+                    {
+                        Charateristics.map(Solution => (
+                            <li>
+                                {
+                                    Solution
+                                }
+                            </li>
+                        ))
+                    }
+                </ul>
+            
     </div>
 );
 
