a97d136bf24767eadd32a24b3e7a1f4e1452d64e
Daniel <danielzrust@gmail.com>
2019-02-23 14:38:26 -0700

    Dz week 07 (#24)
    * started updating the presentation slides
    
    * Updated styles and some wording

--- src/app/presentation.tsx
+++ src/app/presentation.tsx
@@ -2,13 +2,7 @@ import * as React from "react";
 
 const Presentation = () => (
     <div>
-        <h1>
-            Presentation
-                 <p>
-                 <iframe style={{height: '565px', width: '962px'}} src="https://olddominion-my.sharepoint.com/personal/dzrus001_odu_edu/_layouts/15/Doc.aspx?sourcedoc={f8761093-7d98-4e70-9aa8-7f710c6beef3}&amp;action=embedview&amp;wdAr=1.7777777777777777" frameBorder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office Online</a>.</iframe>
-                </p>
-        </h1>	
+        <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRvEERreovjDoYaCJzjmx9s1stKMa5GFgYP37D68Gnhnid056bVoh4UriuunA3Sx1tQIKEIih_I5d8K/embed?start=true&loop=true&delayms=30000" frameBorder="0" style={{ width: '80rem', height: '47rem' }} allowFullScreen={true} />
     </div>
 );
-///test///
 export default Presentation;
