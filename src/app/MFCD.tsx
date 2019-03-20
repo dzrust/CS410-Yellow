3358ca5085936e1841bde4f2039f197b53cd91f2
Daniel <danielzrust@gmail.com>
2019-03-19 19:54:15 -0600

    Added in mockups and added in a carousel to show them

--- src/app/MFCD.tsx
+++ src/app/MFCD.tsx
@@ -1,11 +1,17 @@
 import * as React from "react";
 
 const Charateristics = [
-    "The iOS/Android app will talk with the local storage saving various personal information to run the app that doesn’t need to be stored on the DB",
-    "The iOS/Android app will also get information from the API server",
-    "The API server will communicate with the GIS, DB, and Places API",
-    "GIS and the places API will be served by Google’s API’s",
-    "The DB will be a Postgres DB storing user information, restroom information, reviews, and location information to be provided through the API server.",
+    "Local storage will hold information such as: gender, age, bathroom preferences, how long before a user will pee themselves",
+    "The local storage is used so that the user’s personal data does not have to be kept on our DB thus saving the user from unwanted data leakage",
+    "The mobile app will serve interfaces that allow users: rate bathrooms, find bathrooms, leave reviews, owners of restrooms can see stats on their restroom, and realtime data such as: waits, ratings, and reviews",
+    "Mobile app is written in React Native",
+    "Cloud system are hosted in AWS",
+    "AWS Features used: DynamoDB, Lambda, and Cloudfront",
+    "The AWS cloud system will hold all the virtualization needed for the backend",
+    "Lambda service using node.js and express.js will handle all the api calls",
+    "Backend Db will be DynamoDB (no/sql) that will be accessible via AWS library",
+    "Express app will have access to the GIS information given to us via Google’s maps api",
+    "We will also interface with Google’s places api to get vital information such as: commercial locations, recreational locations, parks, other \"expected\" restroom locations"
 ];
 
 const MFCD = ({}) => (
@@ -17,7 +23,7 @@ const MFCD = ({}) => (
             <img src="images/MFCD.png" alt="MFCD chart" className="MFCD-image" />
         </div>
         <div>
-            <ul className="Solution-page-characteristics">
+            <ul className="solution-page-characteristics">
             {
                 Charateristics.map(MFCD => (
                     <li>
